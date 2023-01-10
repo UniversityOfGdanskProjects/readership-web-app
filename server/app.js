06e50ee68f33c3f7df27cf0a3a71b1b4a21fe4d6
Zofia Piesik <piesikzofia09@gmail.com>
2023-01-10 13:23:29 +0100

    Added dotenv dependency and used it in server/app.js to config

--- server/app.js
+++ server/app.js
@@ -1,34 +1,32 @@
+require('dotenv').config()
+
 import morgan from 'morgan';
 import { connect } from 'mongoose';
 import express from 'express';
-import { config } from './config.js';
-const {appSettings ,db}= config;
+
 const app = express();
 import User from './models/users.js';
-// console.log(db.uri);
 
-connect(db.uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then((res) => {
-    app.listen(appSettings.port);
+    app.listen(process.env.PORT);
     console.log('connected to db');
 })
 .catch((err) => console.log(err));
 
 
-console.log(appSettings.name);
+// middleware
 app.use(morgan('dev'));
+app.use((req, res, next)=> {
+    console.log(req.path, req.method);
+    next();
+});
 
 
 // mongoose and mongo sandbox routes
 app.get('/add-user', (req, res) => {
-    const user = new User({
-        username: 'morganus',
-        firstName: 'Morgan',
-        lastName: 'Freeman',
-        email: 'morgan@free.com',
-        dateOfBirth: '2020/02/01',
-        terms: true
-    });
+    const user = new User(req.body);
     user.save()
         .then((result) => {
             res.send(result);
@@ -38,7 +36,7 @@ app.get('/add-user', (req, res) => {
 
 
 // routes
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Welcome aboard - ReaderSHIP')
 });
 
