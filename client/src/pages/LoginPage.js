ff31ed88f065ffa93fbf30288e40e0bcc9282a86
Zofia Piesik <piesikzofia09@gmail.com>
2023-01-30 22:16:36 +0100

    welcome message

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -3,6 +3,8 @@ import { LoginForm } from '../components/login/LoginForm'
 
 export const LoginPage = () => {
     return ( <div>
+        <h1>Welcome in ReaderSHIP - website to search books!</h1>
+        <h2></h2>
         <LoginForm />
     </div> );
 };
